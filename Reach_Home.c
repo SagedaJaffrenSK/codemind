e70fd372ddfd13a4912767e639b5349fc45ab4cc
SagedaJaffrenSK <147475872+SagedaJaffrenSK@users.noreply.github.com>
2023-11-06 11:05:56 +0530

    #include <stdio.h> int main() {     int X, Y;     scanf("%d%d", &X, &Y);     if (5*X >= 1*Y)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Reach_Home.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int X, Y;
+    scanf("%d%d", &X, &Y);
+    if (5*X >= 1*Y)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
