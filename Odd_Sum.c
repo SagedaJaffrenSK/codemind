e351043c907149d210ddd414701bb0935152c218
Sageda Jaffren Shaik <147475872+SagedaJaffrenSK@users.noreply.github.com>
2023-12-20 22:58:10 +0530

    #include <stdio.h> int main() {     int n, i;     scanf("%d", &n);     int arr[n];     for(i=0; i<n; i++)     {         scanf("%d", &arr[i]);     }     int odd_sum=0;     for(i=0; i<n; i++)     {         if(arr[i]%2==1)         {             odd_sum += arr[i];         }     }     printf("%d", odd_sum);     return 0; }

---
+++ Odd_Sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int arr[n];
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int odd_sum=0;
+    for(i=0; i<n; i++)
+    {
+        if(arr[i]%2==1)
+        {
+            odd_sum += arr[i];
+        }
+    }
+    printf("%d", odd_sum);
+    return 0;
+}
