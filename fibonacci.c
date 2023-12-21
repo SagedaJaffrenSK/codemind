01977b11cfce175e8d3608fb12ea76a30c99583f
Sageda Jaffren Shaik <147475872+SagedaJaffrenSK@users.noreply.github.com>
2023-12-21 20:13:36 +0530

    #include <stdio.h> int main() {     int n, i, a=0, b=1, c=a+b;     scanf("%d", &n);     printf("%d %d ", a, b);     for(i=3; i<=n; i++)     {         printf("%d ", c);         a=b;         b=c;         c=a+b;     }     return 0; }

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, a=0, b=1, c=a+b;
+    scanf("%d", &n);
+    printf("%d %d ", a, b);
+    for(i=3; i<=n; i++)
+    {
+        printf("%d ", c);
+        a=b; 
+        b=c;
+        c=a+b;
+    }
+    return 0;
+}
